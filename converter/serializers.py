579361d0b2ecb2f0f5f4fe03356f3f8731687743
Przemysław Rembak <przemek.rembak@gmail.com>
2020-02-24 11:38:21 +0100

    Remove redundant field in ConvertCurrencySerializer

--- converter/serializers.py
+++ converter/serializers.py
@@ -5,6 +5,3 @@ class ConvertCurrencySerializer(serializers.Serializer):
     currency_1 = serializers.CharField(min_length=3, max_length=3)
     currency_2 = serializers.CharField(min_length=3, max_length=3)
     amount = serializers.DecimalField(max_digits=None, decimal_places=2)
-    converted_amount = serializers.DecimalField(max_digits=None,
-                                                decimal_places=2,
-                                                read_only=True)
